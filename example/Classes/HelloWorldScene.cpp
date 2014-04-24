7578700a116e3fe24164d41da3e1ad4b689a9f06
oldanidavide <info@oldanidavide.com>
2014-04-24 16:37:41 +0200

    Update HelloWorldScene.cpp

--- example/Classes/HelloWorldScene.cpp
+++ example/Classes/HelloWorldScene.cpp
@@ -74,9 +74,6 @@ bool HelloWorld::init()
     // add the sprite as a child to this layer
     this->addChild(pSprite, 0);
     
-
-    //OCCMillennialMedia::setAdvVisible(true);
-
     return true;
 }
 
